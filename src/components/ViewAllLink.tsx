656078d1a82d8b110455896f9263651e1cd4754f
Samuel Mensah <samuelmensqh@gmail.com>
2024-05-12 13:40:40 +0000

    Bugfix-006

--- src/components/ViewAllLink.tsx
+++ src/components/ViewAllLink.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function ViewAllLink() {
   return (
     <>
-      <Link to="/#/list-view" className="underline text-lg font-medium">
+      <Link to="/list-view" className="underline text-lg font-medium">
         View all
       </Link>
     </>
